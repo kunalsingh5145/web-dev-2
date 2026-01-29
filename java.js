34424f2425e6c6fade596d4958b779c6d27cd8b6
kunalsingh5145 <pbgamer868@gmail.com>
2026-01-29 11:46:21 +0530

    manipulate DOM elements, add/remove list items

--- java.js
+++ java.js
@@ -1,7 +1,84 @@
-const container = document.querryselector('.container')
-const outer = document.querryselector('.outer')
-const button = document.querryselector('.button')
+// // const container = document.querryselector('.container')
+// // const outer = document.querryselector('.outer')
+// // const button = document.querryselector('.button')
 
-container.addEventlistner('click',()=>{console.log('div')},true)
-container.addEventlistner('click',()=>{console.log('outer')},true)
-container.addEventlistner('click',()=>{console.log('button')},true)
+// // container.addEventlistner('click',()=>{console.log('div')},true)
+// // container.addEventlistner('click',()=>{console.log('outer')},true)
+// // container.addEventlistner('click',()=>{console.log('button')},true)
+// // console.log("new code")
+// setTimeout(() => {
+//     alert("kunal")
+// // }, 2000);
+// setInterval(() => {
+//    alert("kunal ")
+// }, 3000);
+// let num = 1;
+// const interval= setInterval(() => {
+//     console.log(num);
+//     num= num +1;
+
+//     if(num>10){
+//         clearInterval(interval);
+//     }
+// }, 1000);
+// const name= document.querySelector("name")
+// const btn= document.querySelector("btn")
+// const list= document.querySelector("list")
+// btn.addEventListener('click', ()=>{
+//     const li = document. createElement("li")
+//     li.innertext= name.value;
+//     list.appendchild(li)
+//     name.value=""
+// })
+// const name= document.querySelector("#name")
+// const btn= document.querySelector("btn")
+// const list= document.querySelector("list")
+// btn.addEventListener('click', ()=>{
+//     if(name.value===""){
+//         alert ("enter the name!")
+//         return;
+//     }
+//     const li = document. createElement("li")
+//     const dlt= document.createElement('button')
+//     dlt.innerText= "delete"
+//     li.innerText= name.value;
+//     dlt.addEventListener("click",()=>{
+//         list.removeChild(li)
+// })
+// li.appendChild(dlt)
+// list.appendChild(li)
+// name.value=""
+// })
+// const name = document.querySelector("#name")
+// const btn = document.querySelector(".btn")
+// const list = document.querySelector(".list")
+// btn.addEventListener('click',()=>{
+//     if(name.value==""){
+//         alert("enter the name")
+//         return;
+//     }
+//     const li=document.createElement('li')
+//     const dlt = document.createElement('button')
+//     dlt.innerText="delete"
+//     li.innerText=name.value;
+//     dlt.addEventListener('click',()=>{
+//         list.removeChild(li)
+//     })
+//     li.appendChild(dlt)
+//     list.appendChild(li)
+    
+    
+//     name.value=""
+// })
+function  print(num){
+    setInterval(() => {
+        console.log("inside print")
+        num()
+    },2000 );
+    
+
+}
+function callback(){
+    console.log("inside callback")
+}
+print(callback)
